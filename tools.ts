ec73419379eb89085358b7499e5c7c3b080a6e08
Mark Dierolf <mdierolf@cloudquote.io>
2025-12-02 17:57:45 -0800

    Refactor imports to use named exports instead of `default` for improved clarity and consistency.

--- tools.ts
+++ tools.ts
@@ -1,11 +1,11 @@
-import {default as getArticleById} from "./tools/getArticleById.ts";
-import {default as getArticleBySlug} from "./tools/getArticleBySlug.ts";
-import {default as getBody} from "./tools/getBody.ts";
-import {default as listProviders} from "./tools/listProviders.ts";
-import {default as renderBody} from "./tools/renderBody.ts";
-import {default as searchArticles} from "./tools/searchArticles.ts";
-import {default as searchIndexedData} from "./tools/searchIndexedData.ts";
-import {default as uploadArticle} from "./tools/uploadArticle.ts";
+import getArticleById from "./tools/getArticleById.ts";
+import getArticleBySlug from "./tools/getArticleBySlug.ts";
+import getBody from "./tools/getBody.ts";
+import listProviders from "./tools/listProviders.ts";
+import renderBody from "./tools/renderBody.ts";
+import searchArticles from "./tools/searchArticles.ts";
+import searchIndexedData from "./tools/searchIndexedData.ts";
+import uploadArticle from "./tools/uploadArticle.ts";
 
 export default {
   searchIndexedData,
